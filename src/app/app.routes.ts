fe2184765c0b4e427eecc2a1893f3175938398db
agrandin <agrandin@unowhy.com>
2025-09-23 10:51:48 +0200

    feat: add style and rooting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { ListComponent } from './components/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Route de base qui affiche la liste des événements
+  { path: '', component: ListComponent },
+
+  // Route pour la page d'inscription, avec un paramètre dynamique pour l'ID de l'événement
+  { path: 'register/:eventId', component: RegisterComponent },
+
+  // Redirection vers la page d'accueil si l'URL ne correspond à aucune route
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,8 +1,33 @@
-import { Component, inject, signal, ChangeDetectionStrategy, DestroyRef } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  ChangeDetectionStrategy,
+  DestroyRef,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ApiService } from '../../service/api';
+
+export interface Event {
+    id: number;
+    uuid: string;
+    name: string;
+    description: string;
+    start_date: string;
+    end_date: string;
+    available_slots: number;
+    registrations: Registration[];
+}
+
+export interface Registration {
+    uuid: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+}
 
 @Component({
   selector: 'app-list',
@@ -10,23 +35,24 @@ import { ApiService } from '../../service/api';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, RouterLink],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
   private apiService = inject(ApiService);
   private destroyRef = inject(DestroyRef);
-  public events = signal<any[]>([]);
+  public events = signal<Event[]>([]);
 
   constructor() {
-    this.apiService.getEvents()
+    this.apiService
+      .getEvents()
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
-        next: (data: any[]) => {
+        next: (data: Event[]) => {
           this.events.set(data);
         },
         error: (error) => {
           console.error('Erreur lors de la récupération des événements :', error);
-        }
+        },
       });
   }
 }
